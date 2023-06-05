1a7dbefbe4405cf097f5ebc06ec78bce2ee8b846
Abdi-Raheem <raheem129@hotmail.com>
2023-06-05 15:32:32 -0400

    refactored code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,5 @@
-const eqArrays = function(array1,array2){
-  if(array1.length !== array2.length) {
-    return false
-  } 
-  for (let i = 0; i < array1.length; i++){
-    if (array1[i] !== array2[i]){
-      return false;
-    }
-  } 
+const eqArrays = require('./eqArrays');
 
-  return true;
-}  
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log("Arrays are equal!");
@@ -18,4 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual
